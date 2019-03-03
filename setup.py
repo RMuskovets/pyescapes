6b825de5797b373092b187d30e318330cbd30ba6
RMuskovets <rmuskovets@gmail.com>
2019-03-03 12:30:41 +0200

    some refactorings

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="Console ANSI escape sequences",
     version="0.0.1",
     author="RMuskovets",
     author_email="rmuskovets@gmail.com",
     description="A collection of ANSI escape codes",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/RMuskovets/pyescapes",
     packages=setuptools.find_packages(),
     classifiers=[
